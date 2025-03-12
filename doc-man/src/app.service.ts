ccfa16e352fbe7b4280ef323e1392ae9659b1296
Jitusingh Rajpurohit <rjsingh12123@gmail.com>
2025-03-12 18:55:05 +0530

    MicroService Configuration

--- doc-man/src/app.service.ts
+++ doc-man/src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  static getMicroserviceHost(): string {
+    return process.env.MICROSERVICE_HOST ?? 'localhost';
+  }
+
+  static getMicroservicePort(): number {
+    return parseInt(process.env.MICROSERVICE_PORT ?? '3000');
+  }
+
+  static getPort(): number {
+    return parseInt(process.env.PORT ?? '3000');
   }
 }
--- doc-man/src/main.ts
+++ doc-man/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { Transport } from '@nestjs/microservices';
+import { AppService } from './app.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: AppService.getMicroserviceHost(),
+      port: AppService.getMicroservicePort(),
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(AppService.getPort());
 }
 bootstrap();
